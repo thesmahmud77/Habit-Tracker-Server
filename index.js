861a65c3cf5d285e937b694618ae33f129018578
thesmahmud77 <mahmudsazzad77@gmail.com>
2026-03-27 18:10:42 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware--------------------
+app.use(cors());
+app.use(express.json());
+
+// Mongodb-Driver-Info-From here
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.w4xj3al.mongodb.net/?appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+// app.get("/", (req, res) => {
+//   res.send("Server Running");
+// });
+const db = client.db("B12-A10-Habit-Tracker-DB");
+const productsCollection = db.collection("habits");
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // ----------------------------------------------------------------
+    // --------------    MongoDB API            -----------------------
+    // ----------------------------------------------------------------
+    app.post("/habits", async (req, res) => {
+      const NewData = req.body;
+      const result = await productsCollection.insertOne(NewData);
+      res.send(result);
+    });
+
+    // ----------------------------------------------------------------
+    // --------------    MongoDB API            -----------------------
+    // ----------------------------------------------------------------
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`Server runing from port: ${port}`);
+});
