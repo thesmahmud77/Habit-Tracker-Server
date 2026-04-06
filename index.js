471ee2ef9529e767996a843d760abde4886ae2bd
thesmahmud77 <mahmudsazzad77@gmail.com>
2026-04-06 23:34:01 +0600

    MY havit Delete Funcationlity Update

--- index.js
+++ index.js
@@ -93,6 +93,13 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/my-habits/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const result = await habitCollection.deleteOne(filter);
+      res.send(result);
+    });
+
     // ----------------------------------------------------------------
     // --------------    MongoDB API            -----------------------
     // ----------------------------------------------------------------
